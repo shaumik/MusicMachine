939e188ef78385ca66d0b3bba7d40fab9618ea9c
Jerry Lung <jlung@airbears2-10-142-38-58.airbears2.1918.berkeley.edu>
2014-09-27 19:13:28 -0700

    no index wanted

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.post('/music_search',function(request, response){
 		//response.writeHead(200, {"Content-Type": "text/plain"});
 		//response.write(music_code);
 		//response.end();
+		music_code = "sP4yyXlIhsU";
 		ytdl("http://www.youtube.com/watch?v=" + music_code).pipe(fs.createWriteStream("test.flv"));
 	});
 	// response.setHeader("Content-Type", "text/html");
