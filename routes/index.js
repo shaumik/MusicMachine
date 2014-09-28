76eb570414db96f194413beb78b2a330cf845ea2
Shaumik <shaumik@berkeley.edu>
2014-09-28 15:54:35 -0700

    added database entry. still need to make sure we can update and all that stuff

--- routes/index.js
+++ routes/index.js
@@ -17,29 +17,22 @@ router.get('/', function(request, response) {
 router.post('/music_search',function(request, response){
 	var query_request = request.body.music;	
 	query(query_request, function(music_code,title){
-		//response.setHeader("Content-Type", "text/html");
-		//response.writeHead(200, {"Content-Type": "text/plain"});
-		//response.write(music_code);
-		//response.end();
 		title6 = title;
 		console.log("TITLE", title);
 		ytdl("http://www.youtube.com/watch?v=" + music_code).pipe(fs.createWriteStream("public/output_music/" + query_request + ".mp3")).on("close", function() {
 			response.render('result.html', { titles : title6, query_strings : query_request});
 		});
+		if(request.db){
+			var db = request.db 
+			var collection = db.collection('musicCollection').insert({"code":music_code},function(error,doc){
+				if(error){
+					console.log(error);
+				}else{
+					console.log("Successfully stored data");
+				}
+			});
+		}
 	});
-	// response.setHeader("Content-Type", "text/html");
-	// response.writeHead(200, {"Content-Type": "text/plain"});
-	// response.write(music_code);
-	// response.end();
-
-	// var db = request.db;
-	// var collection = db.collection('musicCollection').insert({"code":music_code},function(error,doc){
-	// 	if (error) {
-	// 		console.log(error)
-	// 	} else {
-	// 		console.log("successfully stored data");
-	// 	}
-	// });	
 	
 });
 
